1256a8bdea7a31055de03b1988751cb2f37be33a
Yana <116554808+Yana-Work@users.noreply.github.com>
2023-07-06 19:03:27 +0300

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,10 +1,10 @@
 <?php
 include_once "database.php";
 
-$host = "192.168.5.249";
-$username = "colnect";
-$password = "Happy_Colnecting";
-$database = "counter";
+$host = "localhost";
+$username = "id20978983_colnect";
+$password = "Od$1m4mq]<$2BRU3";
+$database = "id20978983_counter";
 
 $db = new Database($host, $username, $password, $database);
 
